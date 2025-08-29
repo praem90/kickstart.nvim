2d26b9553d909ec337756aa84dc56da3ae206819
Mohan Raj <praem1990@gmail.com>
2025-08-29 18:34:54 +0530

    Add comments

--- lua/kickstart/plugins/db.lua
+++ lua/kickstart/plugins/db.lua
@@ -15,16 +15,22 @@ return {
     init = function()
       -- Your DBUI configuration
       vim.g.db_ui_use_nerd_fonts = 1
+
+      --- Disable folding for .dbout files
       vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufEnter' }, {
         pattern = '*.dbout',
         callback = function()
           vim.api.nvim_set_option_value('foldenable', false, {})
         end,
       })
+
+      -- Create a new tab with DBUI
       vim.keymap.set('n', '<leader>du', function()
         vim.cmd.tabnew()
         vim.cmd.DBUIToggle()
       end, {})
+
+      -- Close the DBUI tab
       vim.keymap.set('n', '<leader>dc', function()
         vim.cmd.tabclose()
         vim.cmd.DBUIToggle()
